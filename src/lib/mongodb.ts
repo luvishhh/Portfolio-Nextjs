4b984eaf2d0302764a15a5870cdb39edd187eb77
Lavish K <lavishkhare11@gmail.com>
2025-06-15 09:11:40 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,3 +1,4 @@
+
 // @/src/lib/mongodb.ts
 import { config as dotenvConfig } from 'dotenv';
 dotenvConfig(); // Load environment variables from .env file
@@ -34,4 +35,10 @@ if (process.env.NODE_ENV === 'development') {
   clientPromise = globalWithMongo._mongoClientPromise;
 } else {
   // In production mode, it's best to not use a global variable.
-  client = new MongoClient(
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+}
+
+// Export a module-scoped MongoClient promise. By doing this in a
+// separate module, the client can be shared across functions.
+export default clientPromise;
