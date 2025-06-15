977587ce1617ae921ecfdf7d86a8706bcc6e7132
Lavish K <lavishkhare11@gmail.com>
2025-06-15 08:25:13 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,7 @@ const nextConfig: NextConfig = {
         tls: false,
         net: false,
         dns: false,
+        "timers/promises": false, // Added timers/promises
       };
     }
     return config;
